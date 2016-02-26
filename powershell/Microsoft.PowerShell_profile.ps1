384b49da69ac6dde2040c6216bf441ee03b07e2b
Michael Astrauckas <mastrauckas@hotmail.com>
2016-02-26 08:00:06 -0500

    Tweaked Powershell script.

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,30 +1,33 @@
 ##Information on getting Posh-Git for Powershell go to:
 #http://haacked.com/archive/2011/12/13/better-git-with-powershell.aspx/
 #and http://haacked.com/archive/2011/12/19/get-git-for-windows.aspx/
-
-$env:path += ";C:\Git\cmd"
-$env:path += ";C:\Git\bin;C:\Git\usr\bin"
-
+ 
+$env:path += ";C:\Tools\Git\cmd"
+$env:path += ";C:\Tools\Git\bin;C:\Git\usr\bin"
+$env:path += ";C:\Tools\MongoDB\Server\3.2\bin"
+ 
 #Setting this for whatever reason is not making msysgit
 #so I went in and att the envirnment virable in Windows
 #Remove-Variable -Force HOME
+#Windows 10 you get an error when trying to set HOME but setting it in Windows 10 instead of here works.
+#To look at git profile it's at C:\Git\etc\profile
 #Set-Variable HOME "C:\Git\home\<user>"
 #Set-Variable ~ $HOME
-(Get-PSProvider 'FileSystem').Home = "C:\Git\home\<user>"
-
-Set-Location C:\SourceControl\git
+(Get-PSProvider 'FileSystem').Home = "C:\Tools\Git\home\<user>"
+ 
+Set-Location C:\Projects
 $Global:CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent()
 $UserType = "User"
 $CurrentUser.Groups | foreach { if($_.value -eq "S-1-5-32-544") {$UserType = "Admin"} }
 New-Alias Open Invoke-Item
-function cdprojects { Set-Location C:\projects }
+function cdp { Set-Location C:\Projects }
 #This add posh-git like a command prompt and stuff. 
 #However, I want to set my own command prompt.
 # Load posh-git example profile
 . 'C:\Users\<user>\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 #Set-PSReadlineOption -TokenKind Parameter -ForegroundColor Blue
 Start-SshAgent -Quiet
-
+ 
 function prompt {
 	if($UserType -eq "Admin") {
 		$host.ui.rawui.WindowTitle = "" + $(get-location) + " : Admin"
@@ -41,7 +44,7 @@ function prompt {
 	Write-Host ($(Get-Location)) -f Yellow -nonewline;
 	Write-Host ("@") -f Magenta -nonewline;
 	Write-Host ("{0:yyyy:MM:dd-hh:mmtt}" -f (Get-Date)) -f Red -nonewline;
-
+ 
 	if(Test-Path .git) {
 		git branch | foreach {
 			if ($_ -match "^\*(.*)") {
