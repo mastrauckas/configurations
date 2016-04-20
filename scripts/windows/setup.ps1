c29d5a1909c8e112bc18cf44cfde069676e6658e
Michael Astrauckas <mastrauckas@hotmail.com>
2016-04-19 22:59:54 -0400

    No longer need these files.

--- scripts/windows/setup.ps1
+++
@@ -1,9 +0,0 @@
-$dotDirectory = (Get-Item "..\..\dotfiles").FullName
-
-$fromFile = join-path (get-item ".").FullName ".vsvimrc"
-$toFile = join-path (get-item $env:home).FullName ".vsvimrc"
-(Get-Content $fromFile ).Replace("<directory>",$dotDirectory) | Out-File $toFile
-
-$fromFile = join-path (get-item ".").FullName ".vimrc"
-$toFile = join-path (get-item $env:home).FullName ".vimrc"
-(Get-Content $fromFile ).Replace("<directory>",$dotDirectory) | Out-File -Encoding UTF8 $toFile
