e9c8ea46985fb4d7026f128f642f5ac0278a755b
Michael Astrauckas <mastrauckas@hotmail.com>
2016-12-26 08:40:03 -0500

    Add color to all my ls calls like ll.

--- dotfiles/.bash/.global_aliases.bash
+++ dotfiles/.bash/.global_aliases.bash
@@ -1,6 +1,6 @@
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
+alias ll='ls -alFG'
+alias la='ls -AG'
+alias l='ls -CFG'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
