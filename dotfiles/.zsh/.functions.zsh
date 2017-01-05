506adbf2275c6e22040f0fbf869a288c479c66a2
Michael Astrauckas <mastrauckas@hotmail.com>
2017-01-05 00:44:11 -0500

    Making more tweaks and also make it where it puts a hook in and calls my function each time the prompt is generated.

--- dotfiles/.zsh/.functions.zsh
+++ dotfiles/.zsh/.functions.zsh
@@ -1,13 +1,13 @@
-BOLD_GREEN=$fg_bold[green]
-BOLD_BLUE=$fg_bold[blue]
-BOLD_RED=$fg_bold[red]
-LIGHT_CYAN='\e[96m'
-CYAN=$fg[cyan]
-BLUE=$fg[blue]
-YELLOW='\e[1;33m'
-WHITE=$fg[white]
-ORANGE='\e[38;5;214m'
-RESET=$reset_color
+BOLD_GREEN="%{$fg_bold[green]%}"
+BOLD_BLUE="%{$fg_bold[blue]%}"
+BOLD_RED="%{$fg_bold[red]%}"
+LIGHT_CYAN=$'%{\e[96m%}'
+CYAN="%{$fg[cyan]%}"
+BLUE="%{$fg[blue]%}"
+YELLOW=$'%{\e[1;33m%}'
+WHITE="%{$fg[white]%}"
+ORANGE=$'%{\e[38;5;214m%}'
+RESET="%{$reset_color%}"
 
 function getGitVersion {
    IFS=' ' read -a arry <<< "$(git --version)"
@@ -64,39 +64,39 @@ function getCustomPromptWithUnicode {
    local customPrompt
 
    #customPrompt='${debian_chroot:+($debian_chroot)}'
-   customPrompt="${customPrompt}%{$BOLD_GREEN%}➜ "
-   customPrompt="${customPrompt}%{$RESET%}"
-   customPrompt="${customPrompt}%{$LIGHT_CYAN%}%~"
-   customPrompt="${customPrompt}%{$RESET%}"
+   customPrompt="${customPrompt}${BOLD_GREEN}➜ "
+   customPrompt="${customPrompt}${RESET}"
+   customPrompt="${customPrompt}${LIGHT_CYAN}%~"
+   customPrompt="${customPrompt}${RESET}"
 
    local branchName="$(getCurrentGitBranch)"
    if [ ! -z "$branchName" -a "$branchName" != " " ]; then
-      customPrompt="${customPrompt}%{$BOLD_BLUE%} git:("
-      customPrompt="${customPrompt}%{$RESET%}"
+      customPrompt="${customPrompt}${BOLD_BLUE} git:("
+      customPrompt="${customPrompt}${RESET}"
 
       local sha=$(getCurrentGitBranchSha)
       if [ "$branchName" = "$sha" ]; then
-         customPrompt="${customPrompt}%{$BOLD_RED%}DETACHED HEAD"
+         customPrompt="${customPrompt}${BOLD_RED}DETACHED HEAD"
       else
-         customPrompt="${customPrompt}%{$BOLD_RED%}${branchName}"
+         customPrompt="${customPrompt}${BOLD_RED}${branchName}"
       fi
 
-      customPrompt="${customPrompt}%{$RESET%}"
-      customPrompt="${customPrompt}%{$BOLD_BLUE%}:"
-      customPrompt="${customPrompt}%{$RESET%}"
-      customPrompt="${customPrompt}%{$ORANGE%}${sha}"
-      customPrompt="${customPrompt}%{$RESET%}"
-      customPrompt="${customPrompt}%{$BOLD_BLUE%})"
-      customPrompt="${customPrompt}%{$RESET%}"
+      customPrompt="${customPrompt}${RESET}"
+      customPrompt="${customPrompt}${BOLD_BLUE}:"
+      customPrompt="${customPrompt}${RESET}"
+      customPrompt="${customPrompt}${ORANGE}${sha}"
+      customPrompt="${customPrompt}${RESET}"
+      customPrompt="${customPrompt}${BOLD_BLUE})"
+      customPrompt="${customPrompt}${RESET}"
 
       if [ $(isDirty) != 0 ]; then
-         customPrompt="${customPrompt}%{$YELLOW%} ✗"
-         customPrompt="${customPrompt}%{$RESET%}"
+         customPrompt="${customPrompt}${YELLOW} ✗"
+         customPrompt="${customPrompt}${RESET}"
       fi
    fi
 
-   customPrompt="${customPrompt}%{$WHITE%} $ "
-   customPrompt="${customPrompt}%{$RESET%}"
+   customPrompt="${customPrompt}${WHITE} $ "
+   customPrompt="${customPrompt}${RESET}"
 
-   echo "${customPrompt}"
+   PS1="${customPrompt}"
 }
--- dotfiles/.zshrc
+++ dotfiles/.zshrc
@@ -1,4 +1,25 @@
+#http://stackoverflow.com/questions/43321/worth-switching-to-zsh-for-casual-use
+#http://askubuntu.com/questions/825625/how-can-i-extend-the-tab-key-auto-completion-in-the-terminal-to-text-in-the-midd
 autoload -U colors && colors
+autoload -U promptinit && promptinit
+autoload -U zutil
+autoload -U compinit
+compinit
+autoload -U complist
+
+zstyle ':completion:*' matcher-list '' \
+  'm:{a-z\-}={A-Z\_}' \
+  'r:[^[:alpha:]]||[[:alpha:]]=** r:|=* m:{a-z\-}={A-Z\_}' \
+  'r:|?=** m:{a-z\-}={A-Z\_}'
+#autoload -Uz bracketed-paste-magic
+#zle -N bracketed-paste bracketed-paste-magic
+#autoload -Uz url-quote-magic
+#zle -N self-insert url-quote-magic
+
+
+autoload -U compinit
+
+compinit
 
 if [ -f ~/z/z.sh ]; then
    . ~/z/z.sh
@@ -32,5 +53,6 @@ if [ -f ~/.zsh/.local_aliases.zsh ]; then
     . ~/.zsh/.local_aliases.zsh
 fi
 
-PS1="$(getCustomPromptWithUnicode)"
+PROMPT_COMMAND=getCustomPromptWithUnicode
+precmd() { eval "$PROMPT_COMMAND" }
 
