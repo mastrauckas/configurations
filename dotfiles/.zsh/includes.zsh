14ae894ec79fe2a27cc3670f7ca54b3798934654
Michael Astrauckas <michael.a@virtuern.com>
2018-01-07 22:02:04 -0500

    Adding auto complete for kubectl.

--- dotfiles/.zsh/includes.zsh
+++ dotfiles/.zsh/includes.zsh
@@ -5,3 +5,9 @@ fi
 if [ -f /usr/local/etc/profile.d/z.sh ]; then
    . /usr/local/etc/profile.d/z.sh
 fi
+
+if [ -f /usr/local/bin/kubectl ]; then
+   source <(kubectl completion zsh)  # setup autocomplete in zsh
+fi
+
+
