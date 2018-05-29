11b256fd664680a9a8d1f0554e7e2a7e69bd6dfb
Michael Astrauckas <mastrauckas@hotmail.com>
2018-05-28 21:19:04 -0400

    Add alias to remove hardware acceleration for VSCode.

--- dotfiles/.bash/global_aliases.bash
+++ dotfiles/.bash/global_aliases.bash
@@ -2,6 +2,7 @@ alias ll='ls -alFG'
 alias la='ls -AG'
 alias l='ls -CFG'
 alias codei='code-insiders'
+alias codeis='code-insiders --disable-gpu'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
